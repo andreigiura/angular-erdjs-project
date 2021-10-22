1d58ad6db46842afc10b30246fd7fca9e198580b
Andrei Giura <giura.andrei70@gmail.com>
2021-10-22 12:16:14 +0300

    sign on login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SignableMessage } from '@elrondnetwork/erdjs/out';
 import { Signature } from '@elrondnetwork/erdjs/out/signature';
 const ERDJS = require('../assets/erdjs');
 
@@ -9,19 +10,21 @@ const ERDJS = require('../assets/erdjs');
 })
 export class AppComponent {
   title = 'erdjs-angular';
+  signToken = 'ssss';
 
   async login() {
     let provider = ERDJS.ExtensionProvider.getInstance();
     await provider.init();
-    let account = await provider.login();
-    let message = new ERDJS.SignableMessage({
-      message: Buffer.from('TestMe', 'utf8'),
-      address: new ERDJS.Address(account)
-    });
+    await provider.login('', this.signToken);
+    const { signature, address } = provider.account;
 
-    let signed = await provider.signMessage(message);
-    console.log('signed message:', signed);
-    let verifier = ERDJS.UserVerifier.fromAddress(new ERDJS.Address(account));
-    console.log('is signature valid: ', verifier.verify(signed));
+    let toBeValidated = new ERDJS.SignableMessage({
+      message: Buffer.from(`${address}${this.signToken}{}`),
+      address: new ERDJS.Address(address),
+      signature: new Signature(signature)
+    });
+    console.log(toBeValidated);
+    let verifier = ERDJS.UserVerifier.fromAddress(new ERDJS.Address(address));
+    console.log('is signature valid: ', verifier.verify(toBeValidated));
   }
 }
