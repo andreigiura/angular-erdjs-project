78330c0433ad9d1a63277d668088817960263064
Andrei Giura <giura.andrei70@gmail.com>
2021-10-22 07:00:56 +0300

    sign without login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ExtensionProvider } from '@elrondnetwork/erdjs/out';
 import { Signature } from '@elrondnetwork/erdjs/out/signature';
 const ERDJS = require('../assets/erdjs');
 
@@ -9,19 +10,24 @@ const ERDJS = require('../assets/erdjs');
 })
 export class AppComponent {
   title = 'erdjs-angular';
+  accountAddress =
+    'erd1rvzyd3ex5ljll22jmvw8kpugv032fndemv69pg8p5qm42d009n6sr7mytc';
 
   async login() {
-    let provider = ERDJS.ExtensionProvider.getInstance();
+    let provider = ERDJS.ExtensionProvider.getInstance().setAddress(
+      this.accountAddress
+    );
     await provider.init();
-    let account = await provider.login();
     let message = new ERDJS.SignableMessage({
       message: Buffer.from('TestMe', 'utf8'),
-      address: new ERDJS.Address(account)
+      address: new ERDJS.Address(this.accountAddress)
     });
 
     let signed = await provider.signMessage(message);
     console.log('signed message:', signed);
-    let verifier = ERDJS.UserVerifier.fromAddress(new ERDJS.Address(account));
+    let verifier = ERDJS.UserVerifier.fromAddress(
+      new ERDJS.Address(this.accountAddress)
+    );
     console.log('is signature valid: ', verifier.verify(signed));
   }
 }
